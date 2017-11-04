1f960f2cdbc4225d9cf965e0b485d4f655ffd11e
uranio255 <uranio255@gmail.com>
2017-11-04 21:39:45 +0100

    Producir múltiples ficheros CSV
    Ahora se generan 8 ficheros CSV diferentes. Uno por cada fuente contaminante considerada. Cada uno de los CSV sigue teniendo el tiempo como dimensión en el eje horizontal (años entre 1990 y 2015) y país de la Unión Europea como dimensión en el eje vertical.

--- webScraper.py
+++ webScraper.py
@@ -1,3 +1,4 @@
+import os
 import json
 from urllib.request import urlopen
 
@@ -6,8 +7,7 @@ from urllib.request import urlopen
 # -> valoresX: Lista. Contiene todos los valores de que deben aparecer en el eje X de la tabla. La primera fila.
 # -> nombreY: Cadena. Nombre de la variable del eje Y de la tabla.
 # -> valoresY: Lista. Contiene todos los valores de que deben aparecer en el eje Y de la tabla. La primera columna.
-# -> valores: Lista. El resto de valores que forman parte del propio
-# contenido de la tabla.
+# -> valores: Lista. El resto de valores que forman parte del propio contenido de la tabla.
 def construyeTabla(nombreX, valoresX, nombreY, valoresY, valores):
     
     # Crear la tabla.
@@ -37,13 +37,18 @@ def construyeTabla(nombreX, valoresX, nombreY, valoresY, valores):
         
     return tabla
 
-# Esta función toma como entrada una tabla de valores y la escribe en un fichero en disco utilizando el formato CSV (líneas separadas por un retorno de carro y valores separados por comas dentro de una misma línea).
+# Esta función toma como entrada una tabla de valores y la escribe en un fichero en disco utilizando el formato CSV (líneas separadas por un retorno de carro y valores separados por comas dentro de una misma línea). El fichero CSV creado se guardará en un directorio llamado "ficherosCSV" creado justo por debajo de donde está este script.
 # -> tablaDatos: Lista de listas. La tabla de entrada.
 # -> nombreFichero: Cadena. Nombre del fichero que debe escribirse en disco sin extensión ya que se asume ".csv"
 def escribeFicheroCSV(tablaDatos, nombreFichero):
     
+    directorio = "./ficherosCSV"
+    
+    if not os.path.exists(directorio):
+        os.makedirs(directorio)
+    
     # Crear el fichero de salida.
-    ficheroSalida = open(nombreFichero + ".csv", "w")
+    ficheroSalida = open(directorio + "/" + nombreFichero + ".csv", "w")
 
     # Escribir cada línea de la tabla en el fichero CSV.
     for linea in tablaDatos:
@@ -56,26 +61,54 @@ def escribeFicheroCSV(tablaDatos, nombreFichero):
     # Finalmente cerrar el fichero.
     ficheroSalida.close()
 
+# Esta función toma como entrada un código correspondiente a una fuente contaminante y extrae de la web de Eurostat los datos de producción de gases de efecto invernadero referidos a esa fuente para cada uno de los países de la Unión entre 1990 y 2015. Tras haberlos recogido los graba en un fichero CSV cuyo nombre resulta de prefijar la cadena "GreenHouseGases_" y el código de la fuente contaminante.
+# -> codigo: Cadena. Código correspondiente a la fuente contaminante.
+def procesaDatosFuenteContaminante(codigo):
     
+    # URL para obtener los datos de la fuente contaminante en formato JSON de Eurostat
+    url = "http://ec.europa.eu/eurostat/wdds/rest/data/v2.1/json/en/tsdcc210?airemsect=" + codigo
+    
+    # Cargar los datos JSON y los pone en una variable de tipo diccionario.
+    jsonData = json.load(urlopen(url))
+
+    # Obtiene del JSON la lista de valores correspondientes a los años.
+    dimensionTiempo = jsonData["dimension"]["time"]["category"]["label"]
+
+    # Obtiene del JSON la lista de valores correspondientes a los países.
+    dimensionPais = jsonData["dimension"]["geo"]["category"]["label"]
+
+    # Obtiene del JSON la lista del resto de los valores que forman el contenido del dataset.
+    valores = jsonData["value"]
+
+    # Construye la tabla
+    tabla = construyeTabla("Año", dimensionTiempo, "Pais", dimensionPais, valores)
+
+    # Escribe la tabla construida anteriormente en un fichero CSV en disco.
+    escribeFicheroCSV(tabla, "GreenHouseGases_" + codigo)
+
 # Comienzo del programa principal
 
-# La variable URL indica de dónde se toman los datos JSON.
-url = "http://ec.europa.eu/eurostat/wdds/rest/data/v2.1/json/en/tsdcc210?precision=1&airpol=GHG&unit=MIO_T&airemsect=CRF1A3"
+# La variable codigosFuenteContaminante almacena todas las fuentes contaminantes posibles y existentes en el portal Eurostat para el dataset de gases de efecto invernadero producidos en Europa. Estos códigos son importantes para extraer los datos en formato JSON de este portal web.
 
-# Cargar los datos JSON y los pone en una variable de tipo diccionario.
-jsonData = json.load(urlopen(url))
+# Fuentes contaminantes (código - descripción):
+# CRF1-6X4_MEMONIA - All sectors (excluding LULUCF and memo items, including international aviation)
+# CRF1A1 - Fuel combustion in energy industries
+# CRF1A2 - Fuel combustion in manufacturing industries and construction
+# CRF1A3 - Fuel combustion in transport
+# CRF2 - Industrial processes and product use
+# CRF3 - Agriculture
+# CRF5 - Waste management
+# TOTX4_MEMONIA - All sectors and indirect CO2 (excluding LULUCF and memo items, including international aviation)
 
-# Obtiene del JSON la lista de valores correspondientes a los años.
-dimensionTiempo = jsonData["dimension"]["time"]["category"]["label"]
+codigosFuenteContaminante = []
+codigosFuenteContaminante.append("CRF1-6X4_MEMONIA")
+codigosFuenteContaminante.append("CRF1A1")
+codigosFuenteContaminante.append("CRF1A2")
+codigosFuenteContaminante.append("CRF1A3")
+codigosFuenteContaminante.append("CRF2")
+codigosFuenteContaminante.append("CRF3")
+codigosFuenteContaminante.append("CRF5")
+codigosFuenteContaminante.append("TOTX4_MEMONIA")
 
-# Obtiene del JSON la lista de valores correspondientes a los países.
-dimensionPais = jsonData["dimension"]["geo"]["category"]["label"]
-
-# Obtiene del JSON la lista del resto de los valores que forman el contenido del dataset.
-valores = jsonData["value"]
-
-# Construye la tabla
-tabla = construyeTabla("Año", dimensionTiempo, "Pais", dimensionPais, valores)
-
-# Escribe la tabla construida anteriormente en un fichero CSV en disco.
-escribeFicheroCSV(tabla, "GreenHouseGases")
+for codigoFC in codigosFuenteContaminante:
+    procesaDatosFuenteContaminante(codigoFC)
