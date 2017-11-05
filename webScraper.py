6e71df5291318a6bf97b0594086bcfb429d1aeee
uranio255 <uranio255@gmail.com>
2017-11-05 20:14:35 +0100

    Añadida geolocalización para países
    Se han añadido dos columnas más para representar la longitud y la latitud de un punto representativo de cada país. Para obtener estos puntos se utiliza la API de geocodificación de Google.

--- webScraper.py
+++ webScraper.py
@@ -2,6 +2,7 @@ import os
 import json
 import numpy
 from urllib.request import urlopen
+from urllib.parse import quote
 
 # Esta función toma la información de entrada y la transforma en una tabla
 # -> nombreX: Cadena. Nombre de la variable del eje X de la tabla.
@@ -11,13 +12,13 @@ from urllib.request import urlopen
 # -> valores: Lista. El resto de valores que forman parte del propio contenido de la tabla.
 # <- La tabla construída.
 def construyeTabla(nombreX, valoresX, nombreY, valoresY, valores):
-    
+
     # Crear la tabla.
     tabla = []
 
     # Contenido de la esquina superior izquierda de la tabla. Se muestran los nombre de los dos ejes.
     lineaCabecera = [nombreY + "/" +nombreX]
-    
+
     # Poner los valores X en la primera fila de la tabla.
     for x in valoresX:
         lineaCabecera.append(valoresX[x])
@@ -36,21 +37,21 @@ def construyeTabla(nombreX, valoresX, nombreY, valoresY, valores):
 
         tabla.append(linea)
         i += 1
-        
+
     return tabla
 
 # Esta función toma como entrada una tabla de valores y la escribe en un fichero en disco utilizando el formato CSV (líneas separadas por un retorno de carro y valores separados por comas dentro de una misma línea). El fichero CSV creado se guardará en un directorio llamado "ficherosCSV" creado justo por debajo de donde está este script.
 # -> tablaDatos: Lista de listas. La tabla de entrada.
 # -> nombreFichero: Cadena. Nombre del fichero que debe escribirse en disco sin extensión ya que se asume ".csv"
 def escribeFicheroCSV(tablaDatos, nombreFichero):
-    
+
     # Directorio donde se creará el fichero CSV.
     directorio = "./ficherosCSV"
-    
+
     # Asegurar que el directorio existe, si no, crearlo.
     if not os.path.exists(directorio):
         os.makedirs(directorio)
-    
+
     # Crear el fichero de salida.
     ficheroSalida = open(directorio + "/" + nombreFichero + ".csv", "w")
 
@@ -58,10 +59,10 @@ def escribeFicheroCSV(tablaDatos, nombreFichero):
     for linea in tablaDatos:
         for i in range(0, len(tablaDatos[0]) - 1):
             ficheroSalida.write(str(linea[i]) + ",")
-        
+
         ficheroSalida.write(str(linea[len(tablaDatos[0]) - 1]))
         ficheroSalida.write("\n")
-    
+
     # Finalmente cerrar el fichero.
     ficheroSalida.close()
 
@@ -69,44 +70,95 @@ def escribeFicheroCSV(tablaDatos, nombreFichero):
 # Las medidas estadísticas que se hallan son las siguientes: valor máximo y mínimo, media, mediana, desviación estándar y varianza.
 # -> tabla: Lista de listas. La tabla de entrada.
 def agregarColumnasEstadisticas(tabla):
-    
+
     # Agregación del máximo
     tabla[0].append("Valor máximo")
     for i in range(1, len(tabla)):
         tabla[i].append(numpy.max(tabla[i][1:]))
-    
+
     # Agregación del máximo
     tabla[0].append("Valor mínimo")
     for i in range(1, len(tabla)):
         tabla[i].append(numpy.min(tabla[i][1:]))
-    
+
     # Agregación de la media
     tabla[0].append("Media")
     for i in range(1, len(tabla)):
         tabla[i].append(numpy.mean(tabla[i][1:]))
-        
+
     # Agregación de la mediana
     tabla[0].append("Mediana")
     for i in range(1, len(tabla)):
         tabla[i].append(numpy.median(tabla[i][1:]))
-        
+
     # Agregación de la mediana
     tabla[0].append("Desviación estándar")
     for i in range(1, len(tabla)):
         tabla[i].append(numpy.std(tabla[i][1:]))
-        
+
     # Agregación de la mediana
     tabla[0].append("Varianza")
     for i in range(1, len(tabla)):
         tabla[i].append(numpy.var(tabla[i][1:]))
-    
+
+# Esta función geocodifica una localización o dirección textual utilizando la API de Google.
+# -> localizacion: Cadena. Referencia textual a geocodificar.
+# -> geoDiccionario: Diccionario de localizaciones.
+def geoCodifica(localizacion, geoDiccionario):
+
+    # Valor particular no geocodificable.
+    if localizacion == 'European Union (28 countries)':
+        return 0,0
+
+    # Valor particular que debe corregirse antes de geocodificarlo.
+    if localizacion == 'Germany (until 1990 former territory of the FRG)':
+        localizacion = 'Germany'
+
+    # Si la localizacion ha sido geocodificada ya durante el transcurso de la ejecución de este script recuperar longitud y latitud del geo diccionario. Si no, llamar a la API de Google para obtener las coordenadas.
+    if localizacion in geoDiccionario:
+        return geoDiccionario[localizacion]["longitud"], geoDiccionario[localizacion]["latitud"]
+    else:
+
+        # La key a utilizar para llamar a la API de geocodificación de Google.
+        googleAPIKey = "AIzaSyDWMptQo1kPSv3zunxq0fUwEWnFc-NkbFQ"
+
+        # URL de la api de geocodificación de Google
+        url = "https://maps.googleapis.com/maps/api/geocode/json?address=" + quote(localizacion) + "&key=" + googleAPIKey
+
+        # Cargar la respuesta JSON de Google.
+        jsonData = json.load(urlopen(url))
+
+        # Obtener la longitud y latitud.
+        longitud = jsonData["results"][0]["geometry"]["location"]["lng"]
+        latitud = jsonData["results"][0]["geometry"]["location"]["lat"]
+
+        # Actualizar el geo diccionario.
+        geoDiccionario[localizacion] = {"longitud": longitud, "latitud": latitud}
+
+        return longitud, latitud
+
+# Esta función ejecuta una geocodificación de todos los valores presentados en el eje vertical de la tabla. Normalmente deberían ser nombres de países.
+# -> tabla: Lista de listas. La tabla en cuestión.
+# -> geoDiccionario: Diccionario de localizaciones.
+def geoCodificaValoresY(tabla, geoDiccionario):
+
+    # Añade dos columnas nuevas para la longitud y la latitud.
+    tabla[0].append("Longitud")
+    tabla[0].append("Latitud")
+
+    # Rellena ambas columnas.
+    for i in range(1, len(tabla)):
+        longitud, latitud = geoCodifica(tabla[i][0], geoDiccionario)
+        tabla[i].append(longitud)
+        tabla[i].append(latitud)
+
 # Esta función toma como entrada un código correspondiente a una fuente contaminante y extrae de la web de Eurostat los datos de producción de gases de efecto invernadero referidos a esa fuente para cada uno de los países de la Unión entre 1990 y 2015. Tras haberlos recogido los graba en un fichero CSV cuyo nombre resulta de prefijar la cadena "GreenHouseGases_" y el código de la fuente contaminante.
 # -> codigo: Cadena. Código correspondiente a la fuente contaminante.
-def procesaDatosFuenteContaminante(codigo):
-    
+def procesaDatosFuenteContaminante(codigo, geoDiccionario):
+
     # URL para obtener los datos de la fuente contaminante en formato JSON de Eurostat
     url = "http://ec.europa.eu/eurostat/wdds/rest/data/v2.1/json/en/tsdcc210?airemsect=" + codigo
-    
+
     # Cargar los datos JSON y los pone en una variable de tipo diccionario.
     jsonData = json.load(urlopen(url))
 
@@ -124,10 +176,13 @@ def procesaDatosFuenteContaminante(codigo):
 
     # Agregar columnas estadísticas
     agregarColumnasEstadisticas(tabla)
-    
+
+    # Geocodifica los valores dispuestos en la vertical (típicamente países)
+    geoCodificaValoresY(tabla, geoDiccionario)
+
     # Escribe la tabla construida anteriormente en un fichero CSV en disco.
     escribeFicheroCSV(tabla, "GreenHouseGases_" + codigo)
-    
+
 # Comienzo del programa principal
 
 # La variable codigosFuenteContaminante almacena todas las fuentes contaminantes posibles y existentes en el portal Eurostat para el dataset de gases de efecto invernadero producidos en Europa. Estos códigos son importantes para extraer los datos en formato JSON de este portal web.
@@ -152,5 +207,9 @@ codigosFuenteContaminante.append("CRF3")
 codigosFuenteContaminante.append("CRF5")
 codigosFuenteContaminante.append("TOTX4_MEMONIA")
 
+# Inicializa el geo diccionario a utilizar.
+# La idea es utilizarlo con el fin de evitar llamar más de una vez a la API de Google para geocodificar una localización ya geocodificada anteriormente durante el transcurso de la ejecución de este script.
+geoDiccionario = {}
+
 for codigoFC in codigosFuenteContaminante:
-    procesaDatosFuenteContaminante(codigoFC)
+    procesaDatosFuenteContaminante(codigoFC, geoDiccionario)
